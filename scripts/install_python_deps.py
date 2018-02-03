757821a626f05ca317b46df113a4918ec1cb761a
John Vilk <jvilk@cs.umass.edu>
2018-02-03 16:19:58 -0500

    More friendly error message.

--- scripts/install_python_deps.py
+++ scripts/install_python_deps.py
@@ -10,7 +10,10 @@ if sys.version_info < (3,6):
     sys.exit("node-mitmproxy requires a Python version >= 3.6, but found {}.{}".format(sys.version_info[0], sys.version_info[1]))
 
 # Verify that mitmproxy is installed
-check_call(["mitmdump", "--version"])
+try:
+    check_call(["mitmdump", "--version"])
+except FileNotFoundError:
+    sys.exit("node-mitmproxy requires mitmproxy to be installed. See http://docs.mitmproxy.org/en/stable/install.html for instructions.")
 
 # Install dependencies with pip3 first.
 # If pip3 isn't found, use pip.
